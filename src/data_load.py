e4489f04d31f70db079175705e3801a43e430d4a
farhafathima33 <farhafathima3361@gmail.com>
2026-02-25 22:44:16 +0530

    Add load_data stage and source code

---
+++ src/data_load.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import os
+
+def main():
+    print("Loading raw dataset...")
+
+    # Load raw data
+    df = pd.read_csv("data/raw/data.csv")
+
+    print("Dataset shape before cleaning:", df.shape)
+
+    # Basic validation: check for missing values
+    if df.isnull().sum().sum() > 0:
+        print("Missing values found. Dropping...")
+        df = df.dropna()
+    else:
+        print("No missing values found.")
+
+    print("Dataset shape after cleaning:", df.shape)
+
+    # Create processed folder if not exists
+    os.makedirs("data/processed", exist_ok=True)
+
+    # Save cleaned data
+    df.to_csv("data/processed/data.csv", index=False)
+
+    print("Cleaned dataset saved successfully!")
+
+if __name__ == "__main__":
+    main()
