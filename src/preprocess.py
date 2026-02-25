9dfc156d3055f46a58fc9033fc0e82e2f54a2ef6
farhafathima33 <farhafathima3361@gmail.com>
2026-02-25 22:57:26 +0530

    Add preprocess stage

---
+++ src/preprocess.py
@@ -0,0 +1,24 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+def main():
+    print("Loading processed dataset...")
+
+    df = pd.read_csv("data/processed/data.csv")
+
+    print("Splitting into train and test...")
+
+    train, test = train_test_split(
+        df,
+        test_size=0.2,
+        random_state=42,
+        stratify=df["target"]
+    )
+
+    train.to_csv("data/processed/train.csv", index=False)
+    test.to_csv("data/processed/test.csv", index=False)
+
+    print("Train and test datasets saved successfully!")
+
+if __name__ == "__main__":
+    main()
